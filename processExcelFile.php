4b719b03cbc0273276c87e4ab8e1f68c58d06623
Jose Contreras <5625419+GoldenHomer@users.noreply.github.com>
2022-09-03 10:12:02 -0500

    Create processExcelFile.php

---
+++ processExcelFile.php
@@ -0,0 +1,35 @@
+<?php
+  // PHP file below is for parsing Excel file once imported button is clicked
+  require_once('include/SimpleXLSX.php');
+  require_once('include/dbconn.php');
+  
+  // if file was uploaded
+  if (isset($_FILES['file'])) {
+    // if file uploaded is Excel file and can be parsed
+    if ( $xlsx = SimpleXLSX::parse($_FILES['file']['tmp_name']) ) {
+
+      $dim = $xlsx->dimension();
+      $cols = $dim[0];
+
+      // $key => $row is a key-value pair that's easier to read
+      foreach ( $xlsx->rows() as $key => $row ) {
+        if ($key == 0) continue; // skip first row which is the column headers
+
+        // For the acquired date column ($row[8]) , Excel includes hidden time (00:00:00) which isn't needed. So trim the time out.
+        $params = array( $row[0], $row[1] $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], substr($row[8], 0, 10), $row[9] );
+
+        $sql = "INSERT INTO dbo.inventory (tag, description, serial, model, manufacturer, building, room, note, acquiredDate, amount)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
+
+        $execute = sqlsrv_query($conn, $sql, $params);
+      }
+    } 
+    else
+      error_log(SimpleXLSX::parseError());
+  }
+  
+  sqlsrv_close($conn);
+  
+  header([REMOVED URL FOR PRIVACY]);
+  exit();
+?>
